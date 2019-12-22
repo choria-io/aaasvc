1c8bbe7dbf1d4472564c339af0c72cb61931dc7e
R.I.Pienaar <rip@devco.net>
2019-12-22 16:00:55 +0100

    (misc) simplify docker build.sh

--- packager/templates/docker/build.sh
+++ packager/templates/docker/build.sh
@@ -2,14 +2,10 @@
 
 set -x
 
-yum -y install docker-client
-
 cd "{{cpkg_name}}-{{cpkg_version}}"
 
-cp dist/Dockerfile .
-
-docker build . --tag "{{cpkg_user}}/{{cpkg_name}}:{{cpkg_version}}" --tag "{{cpkg_user}}/{{cpkg_name}}:latest"
+docker build -f dist/Dockerfile --tag "{{cpkg_user}}/{{cpkg_name}}:{{cpkg_version}}" --tag "{{cpkg_user}}/{{cpkg_name}}:latest" .
 
 mkdir /containers
 
-docker save "{{cpkg_user}}/{{cpkg_name}}:{{cpkg_version}}" > "/containers/{{cpkg_name}}-{{cpkg_version}}.tar"
+docker save "{{cpkg_user}}/{{cpkg_name}}:{{cpkg_version}}" > "/containers/{{cpkg_name}}-{{cpkg_version}}-docker.tar"
